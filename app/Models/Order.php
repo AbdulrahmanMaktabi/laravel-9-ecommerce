2cd62a70f6089620ac811e660d5acaf670acb656
Abdulrahman <77457323+AbdulrahmanMaktabi@users.noreply.github.com>
2025-05-08 23:26:09 +0300

    Order model - not completed

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,10 +2,50 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Order extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['created_at', 'updated_at', 'id'];
+
+    public function store()
+    {
+        return $this->belongsTo(Store::class);
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(OrderItem::class, 'order_items')
+            ->withPivot(['product_name', 'price', 'qty', 'options']);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public static function booted()
+    {
+        static::creating(function (Order $order) {
+            $order->number = Order::getOrderNumber();
+        });
+    }
+
+    public static function getOrderNumber()
+    {
+        $year = Carbon::now()->year;
+
+        $number = Order::whereYear('created_at', $year)
+            ->max('number');
+
+        if (!$number)
+            return $year . "0001";
+
+        return $number + 1;
+    }
 }
